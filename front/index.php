5e195c8046c78c56b29b1e344188c0de168cc5e9
geek-samarth <samarthgeek435@gmail.com>
2024-02-22 17:54:02 +0530

    v1.6

--- front/index.php
+++ front/index.php
@@ -1,15 +1,18 @@
 <?php
 add_action('init', 'wspc_shop_page_customizer_option_class', 2  );
+
 require_once( WSPC_PLUGIN_DIR_PATH .'front/product-loop-setting.php');
 require_once( WSPC_PLUGIN_DIR_PATH .'front/shop-page-setting.php');
 
 function wspc_shop_page_customizer_option_class(){
+    if( !is_admin() ) {
+        $product_loop_obj  = new wspc_front_product_loop_settings();
+        $product_loop_obj->wspc_customizations_loop();
+    
 
-    $product_loop_obj  = new wspc_front_product_loop_settings();
-    $product_loop_obj->wspc_customizations_loop();
-   
-
-    $shop_page_obj  = new wspc_front_shop_page_settings();
-    $shop_page_obj->wspc_customizations_shop_page();
+        $shop_page_obj  = new wspc_front_shop_page_settings();
+        $shop_page_obj->wspc_customizations_shop_page();
+    }
+    
 }
 ?>
