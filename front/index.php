ae66eefdebdae1bf96d93fb41dc32959b8894a8f
samarth geek <74664988+geek-samarth@users.noreply.github.com>
2024-02-22 17:58:37 +0530

    v1.6

--- front/index.php
+++ front/index.php
@@ -8,7 +8,6 @@ function wspc_shop_page_customizer_option_class(){
     if( !is_admin() ) {
         $product_loop_obj  = new wspc_front_product_loop_settings();
         $product_loop_obj->wspc_customizations_loop();
-    
 
         $shop_page_obj  = new wspc_front_shop_page_settings();
         $shop_page_obj->wspc_customizations_shop_page();
